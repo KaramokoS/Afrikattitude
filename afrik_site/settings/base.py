4d8aec942515f225139b41fa0aef53494d51ded2
Kara SAMASSA <samss.kara@gmail.com>
2020-05-28 00:49:30 +0200

    correcting heroku error

--- afrik_site/settings/base.py
+++ afrik_site/settings/base.py
@@ -155,12 +155,12 @@ CACHE_MIDDLEWARE_KEY_PREFIX = 'afrik_site'
 
 
 # Let's encrypte from  https://www.codingforentrepreneurs.com/blog/ssltls-settings-for-django/
-CORS_REPLACE_HTTPS_REFERER      = False
-HOST_SCHEME                     = "http://"
-SECURE_PROXY_SSL_HEADER         = None
-SECURE_SSL_REDIRECT             = False
-SESSION_COOKIE_SECURE           = False
-CSRF_COOKIE_SECURE              = False
-SECURE_HSTS_SECONDS             = None
-SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
-SECURE_FRAME_DENY               = False
+# CORS_REPLACE_HTTPS_REFERER      = False
+# HOST_SCHEME                     = "http://"
+# SECURE_PROXY_SSL_HEADER         = None
+# SECURE_SSL_REDIRECT             = False
+# SESSION_COOKIE_SECURE           = False
+# CSRF_COOKIE_SECURE              = False
+# SECURE_HSTS_SECONDS             = None
+# SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+# SECURE_FRAME_DENY               = False
